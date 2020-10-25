41f2d8563712162b2a29b3c77f880e706e288eab
maximilianoromeror <lromeror1801@alumno.ipn.mx>
2020-10-25 04:30:19 -0600

    Correccion de Prueba Hola Mundo

--- Hola Mundo/Holamundo.java
+++ Hola Mundo/Holamundo.java
@@ -1,31 +1,5 @@
-class HolaMundo{
-    //para poder hacer un comentario sobre la linea de codigo
-    /*
-    para poder realizar comentarios
-    en diferentes lines de codigo
-    y asi escribir todo lo que querramos y no afectara al codigo
-    principal
-    
-    dos tipos de programadores
-    class nombreclase{
-    }
-    class nombreclase
-    {
-        //es el cuerpo del programa, aqui van todas las instrucciones
-        operaciones, metodos, varibles etc
-    }
-    */ 
-
+class Holamundo{
     public static void main(String[] args){
-        //una declaracion, una idea??, orden 
-        //estructura base de cualquiera clase principal
-
-        //inicializacion de varibales
-        String nombre = "Cosita kawaii";
-        //escribir "la oracion"
-        //clase.objeto.metodo
-        System.out.println("Por fis di Hola mundo");
-        System.out.println(nombre);
+        System.out.println("Hola mundo");
     }
-
 } 
